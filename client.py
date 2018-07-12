cde491a9adee2a289ed8a4242f23fefad5d34059
Manik Narang <mnarang@miteksystems.com>
2018-07-12 16:49:47 -0700

    Running server/client

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import requests
 
 img = open("test.jpg", 'rb').read()
-headers = {'Type': 'Baseline'}
+headers = {'Type': 'Baseline', 'Image': 'LOL'}
 r = requests.post("http://10.202.0.179:8100", data=img, headers=headers)
 
 # And done.
--- server.py
+++ server.py
@@ -4,13 +4,22 @@ import pickle
 app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def result():
-    request.headers['Type'] # should display 'bar'
+    #print(request.headers) # should display 'bar'
     print(len(request.data))
 
-    outfile = open("img.jpg", 'wb')  # Open a file for binary write
-    outfile.write(request.data)  # Write it
-    outfile.flush()  # Optional but a good idea
-    outfile.close()
+    if request.headers['Type'] == 'Baseline':
+        fh = open("baseline.jpg", 'wb')
+        fh.write(request.headers['Image'].decode('base64'))
+        fh.close()
+    
+    fh = open("imageToSave.jpg", "wb")
+    fh.write(request.headers['Image'].decode('base64'))
+    fh.close()
+
+    #outfile = open("img.jpg", 'wb')  # Open a file for binary write
+    #outfile.write(request.data)  # Write it
+    #outfile.flush()  # Optional but a good idea
+    #outfile.close()
 
     return 'Received !' # response to your request.
 

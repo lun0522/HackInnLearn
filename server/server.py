548cf4233fcbdffaec19f468b4c40d27be0cacb3
Manik Narang <mnarang@miteksystems.com>
2018-07-13 12:01:17 -0700

    Logs on server side

--- server/server.py
+++ server/server.py
@@ -8,9 +8,19 @@ import face_recognition
 
 app = Flask(__name__)
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 @app.route('/', methods=['POST'])
 def result():
-    print("Length of the data: ", len(request.data))
+    #print("Length of the data: ", len(request.data))
 
     if not os.path.isfile("//Users//mnarang//hack//server//baseline.jpg"):
         fh = open("//Users//mnarang//hack//server//baseline.jpg", 'wb')
@@ -28,14 +38,13 @@ def result():
 
     # Check if the unknown image is a face
     faces = face_recognition.face_locations(unknown_image)
-    print("Number of faces in the frame: ", faces)
+    print("Faces in the frame: ", faces)
 
     biden_encoding = face_recognition.face_encodings(known_image)[0]
     if len(faces) != 0:
         unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
 
         results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
-        print("Result: " , results[0], "\n\n")
 
         ''' Only uncomment when running client.
         outfile = open("img.jpg", 'wb')  # Open a file for binary write
@@ -45,9 +54,14 @@ def result():
 
         ret = results[0]
         ret = str(ret)
+        if ret == "True":
+            print(bcolors.BOLD + bcolors.UNDERLINE + bcolors.OKGREEN + "(Result: " + "True - User authenticated)\n" + bcolors.ENDC)
+        else:
+            print(bcolors.BOLD + bcolors.UNDERLINE + bcolors.FAIL + "(Result: " + "False - Hibernating app)\n" + bcolors.ENDC)
+
         return ret
 
-    print("(Result: ', No face)\n\n")
+    print(bcolors.BOLD + bcolors.UNDERLINE + bcolors.WARNING + "(Result: " + "No face - Hibernating app)\n" + bcolors.ENDC)
     return "No face"
 
 app.run("0.0.0.0", port=8100)
